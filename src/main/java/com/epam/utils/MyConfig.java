6aabf0315ba69ffdc6d620a61b90f9c408fdb010
Dmitry_Lebedev1 <Dmitry_Lebedev1@epam.com>
2018-06-08 14:18:53 +0300

    add form

--- src/main/java/com/epam/utils/MyConfig.java
+++ src/main/java/com/epam/utils/MyConfig.java
@@ -2,7 +2,7 @@ package com.epam.utils;
 
 import org.aeonbits.owner.Config;
 
-import static org.aeonbits.owner.Config.*;
+import static org.aeonbits.owner.Config.Sources;
 
 @Sources("classpath:my.properties")
 public interface MyConfig extends Config {
